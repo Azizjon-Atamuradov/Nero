2ff24382a12dce63fe146d8e77f98b30b752ec1e
Aziz <azizatamuradov1994@gmail.com>
2026-03-31 08:35:34 -0400

    Add NewArrivals header and carts

--- src/components/NewArrivals/NewArrivals.jsx
+++ src/components/NewArrivals/NewArrivals.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import { products } from "../../data/products";
 import "./NewArrivals.css";
 
 const NewArrivals = () => {
   return (
     <div className="arrivals-container">
-      <div className="arrivals-text">
+      <div className="arrivals-header">
         <h1> NEW ARRIVALS</h1>
         <p>Discover the latest styles just dropped this week.</p>
+      </div>
+
+      <div className="products-grid">
+        {products.map((product) => (
+          <div key={product.id} className="product-card">
+            <img src={product.img} alt={product.name} />
+            <div className="product-details">
+              
+                    <h3>${product.price}</h3>
+                    <p>{product.name}</p>
+            </div>
+
+            {/* <button>View Product</button> */}
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/data/products.js
@@ -0,0 +1,55 @@
+import product1 from "../assets/product1.jpg"
+import product2 from "../assets/product2.jpg"
+import product3 from "../assets/product3.jpg"
+import product4 from "../assets/product4.jpg"
+
+export const products = [
+    {
+        id: 1,
+        name: "Cool Sneakers",
+        price: 50,
+        img: product1,
+    },
+    {
+        id: 2,
+        name: "Running Shoes",
+        price: 60,
+        img: product2,
+    },
+    {
+        id: 3,
+        name: "Stylish Jacket",
+        price: 70,
+        img: product3,
+    },
+    {
+        id: 4,
+        name: "Classic Hoodie",
+        price: 80,
+        img: product4,
+    },
+    {
+        id: 5,
+        name: "Classic Hoodie",
+        price: 80,
+        img: product4,
+    },
+    {
+        id: 6,
+        name: "Classic Hoodie",
+        price: 80,
+        img: product4,
+    },
+    {
+        id: 7,
+        name: "Classic Hoodie",
+        price: 80,
+        img: product4,
+    },
+    {
+        id: 8,
+        name: "Classic Hoodie",
+        price: 80,
+        img: product4,
+    },
+]
