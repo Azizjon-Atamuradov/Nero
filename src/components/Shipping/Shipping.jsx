03108d00f3c142708e227999a106fa3a5a3d2da2
Aziz <azizatamuradov1994@gmail.com>
2026-04-05 10:00:30 -0400

    Add style for Shipping and Navbar components

--- src/components/Shipping/Shipping.jsx
+++ src/components/Shipping/Shipping.jsx
@@ -1,37 +1,30 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import "./Shipping.css";
 
-
-const messages = [
-  "FREE SHIPPING ON ORDERS $70+",
-  "FREE AND EASY RETURNS",
-];
+const messages = ["FREE SHIPPING ON ORDERS $70+", "FREE AND EASY RETURNS"];
 
 const Shipping = () => {
-
   const [index, setIndex] = useState(0);
   const [fade, setFade] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setFade(false)
+      setFade(false);
 
       setTimeout(() => {
         setIndex((prev) => (prev + 1) % messages.length);
-        setFade(true)
-      }, 300)
-    }, 5000)
-    return () => clearInterval(interval)
+        setFade(true);
+      }, 300);
+    }, 5000);
+
+    return () => clearInterval(interval);
   }, []);
-
-
-
 
   return (
     <div className="shipping-container">
-      <p className={fade ? "fade-in" : "fade out"}>{messages[index]}</p>
+      <p className={fade ? "fade-in" : "fade-out"}>{messages[index]}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Shipping
+export default Shipping;
