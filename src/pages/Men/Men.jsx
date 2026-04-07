c431b0bb589fba542f1e26eb3c212d85370b1d11
Aziz <azizatamuradov1994@gmail.com>
2026-04-07 05:07:34 -0400

    Add Load More button and functionality

--- src/pages/Men/Men.jsx
+++ src/pages/Men/Men.jsx
@@ -1,8 +1,14 @@
 import "./Men.css";
 
 import { products } from "../../data/products";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Men = () => {
   const menProducts = products.filter((item) => item.category === "men");
+  const [visibleCount, setVisibleCount] = useState(12);
+  const visibleProducts = menProducts.slice(0, visibleCount);
+  const navigate = useNavigate();
 
   return (
     <div className="men">
@@ -15,26 +21,32 @@ const Men = () => {
       </div>
 
       <div className="men-grid">
-        {menProducts.map((product) => (
-          <div className="product-card" key={product.id}>
+        {visibleProducts.map((product) => (
+          <div
+            className="product-card"
+            key={product.id}
+            onClick={() => navigate(`/product/${product.id}`)}
+          >
             <div className="product-image">
               <img src={product.img} alt={product.name} />
             </div>
 
             <div className="product-info">
-                 <h3 className="product-price">${product.price}</h3>
+              <h3 className="product-price">${product.price}</h3>
               <p className="product-name">{product.name}</p>
-           
             </div>
           </div>
         ))}
       </div>
+      {visibleCount < menProducts.length && (
+        <div className="view-all">
+          <button onClick={() => setVisibleCount((prev) => prev + 12)}>
+            Load More
+          </button>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Men;
-
- 
-
- 
